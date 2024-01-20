98de326484f9b88e4dd5f95a741964d982748472
dbl-hoo <125836384+dbl-hoo@users.noreply.github.com>
2024-01-20 12:04:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,54 +16,56 @@ create_and_mount_partitions() {
   echo "Make sure to identify the existing partitions on $DRIVE."
 
   # Create a new 100GB ext4 partition after the existing root partition
-  NEW_PARTITION="$(parted -s $DRIVE mkpart primary ext4 0% 100GB | grep -oE '/dev/[^ ]+')"
+  cfdisk
+
+  read -p "Enter the name of the root partition for install (e.g., /dev/nvme0n1p4): " ROOT
 
   # Format the new partition
-  mkfs.ext4 $NEW_PARTITION
+  mkfs.ext4 $ROOT
 
   # Mount the new partition
-  mount $NEW_PARTITION /mnt
+  mount $ROOT /mnt
 }
 
-# Function to configure wireless network
-configure_wireless_network() {
-  # Prompt for wireless network information
-  read -p "Enter the SSID (network name): " SSID
-  read -s -p "Enter the password for $SSID: " PASSWORD
-  echo
-
-  # Configure wireless connection using wpa_supplicant
-  wpa_passphrase "$SSID" "$PASSWORD" > /mnt/etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-}
 
 # Function to perform chroot setup
 perform_chroot_setup() {
+  #set the clock
+  sv up ntpd
+  
   # Basestrap essential packages and network tools for Artix Linux
-  basestrap /mnt base base-devel runit elogind-runit os-prober
+  basestrap /mnt base base-devel runit elogind-runit linux linux-firmware
 
   # Generate fstab
   fstabgen -U /mnt >> /mnt/etc/fstab
 
-  # Prompt for hostname
+  #  Configure network (adjust accordingly)
   read -p "Enter the desired hostname: " HOSTNAME
   echo "$HOSTNAME" > /mnt/etc/hostname
+  echo "127.0.0.1 localhost
+  ::1       localhost
+  127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" > /mnt/etc/hosts
 
+  # set the timezone
+  artix-chroot /mnt ln -sf /usr/share/zoneinfo/Americas/New_York /etc/localtime
+
+  #Run hwclock to generate /etc/adjtime:
+  artix-chroot /mnt hwclock --systohc
+
+  #set the locale
+  artix-chroot /mnt echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
+  artix-chroot /mnt locale.gen
+  artix-chroot /mnt echo LANG=en_US.UTF-8 >> /etc/locale.conf
+  
   # Prompt for username
   read -p "Enter the desired username: " USERNAME
 
   # Configure network (install and enable NetworkManager)
-  artix-chroot /mnt pacman -S networkmanager wpa_supplicant
+  artix-chroot /mnt pacman -S networkmanager networkmanger-runit
   artix-chroot /mnt ln -s /etc/runit/sv/NetworkManager /etc/runit/runsvdir/default/
 
-  # Set up wpa_supplicant in chroot
-  cp /etc/wpa_supplicant/wpa_supplicant-wlan0.conf /mnt/etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-
-  # Connect to the wireless network
-  artix-chroot /mnt wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-  artix-chroot /mnt dhcpcd wlan0
-
   # Install and configure bootloader (GRUB in this example)
-  artix-chroot /mnt pacman -S grub os-prober
+  artix-chroot /mnt pacman -S grub os-prober 
 
   # Identify the EFI partition and install GRUB
   EFI_PARTITION="$(lsblk -f $DRIVE | grep 'EFI System Partition' | awk '{print $1}')"
@@ -79,14 +81,16 @@ perform_chroot_setup() {
   echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
 
   # Set the root password
+  echo "Enter the new root password: "
   artix-chroot /mnt passwd
 
   # Create a user and add to wheel group for sudo access
   artix-chroot /mnt useradd -m -g users -G wheel -s /bin/bash $USERNAME
+  echo "Enter the new user user password: "
   artix-chroot /mnt passwd $USERNAME
 
   # Optional: Install additional software
-  # artix-chroot /mnt pacman -S your-package
+  artix-chroot /mnt pacman -S nano 
 
   # Unmount partitions
   umount -R /mnt
@@ -97,5 +101,4 @@ perform_chroot_setup() {
 # Main script
 print_disk_info
 create_and_mount_partitions
-configure_wireless_network
 perform_chroot_setup
