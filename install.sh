eb7b65d94fd74d07091fe8d42d0f9da75b892036
dbl-hoo <125836384+dbl-hoo@users.noreply.github.com>
2024-01-20 06:35:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,66 +1,82 @@
 #!/bin/bash
 
-# Prompt for username
-read -p "Enter the desired username: " USERNAME
+# List available drives using lsblk
+lsblk
+echo "Make sure to identify the correct drive for partitioning."
 
-# Prompt for password
-read -s -p "Enter the password for $USERNAME: " PASSWORD
-echo
+# Prompt for the drive to partition
+read -p "Enter the drive to partition (e.g., /dev/nvme0n1): " DRIVE
 
+# List existing partitions on the selected drive
+lsblk $DRIVE
+echo "Make sure to identify the existing partitions on $DRIVE."
 
-# Define variables
-ROOT_PARTITION="/dev/nvme0n1p3"  # Replace with your root partition
-EFI_PARTITION="/dev/nvme0n1p1"  # Replace with your existing EFI partition
-HOSTNAME="artix"
-PACKAGES_FILE="packages.txt"
+# Create a new 100GB ext4 partition after the existing root partition
+NEW_PARTITION="$(parted -s $DRIVE mkpart primary ext4 0% 100GB | grep -oE '/dev/[^ ]+')"
 
-# Format existing partitions
-mkfs.vfat -F32 $EFI_PARTITION
-mkfs.ext4 $ROOT_PARTITION
-
-# Create and format the new ext4 partition using parted starting at the first available block
-parted -s /dev/nvme0n1 mkpart primary ext4 0% 100GiB
+# Format the new partition
 mkfs.ext4 $NEW_PARTITION
 
-# Mount partitions
-mount $ROOT_PARTITION /mnt
-mkdir -p /mnt/boot
-mount $EFI_PARTITION /mnt/boot
+# Mount the new partition
+mount $NEW_PARTITION /mnt
 
-# Install base system with OpenRC and additional packages from packages.txt
-basestrap /mnt base base-devel runit elogind-runit $(cat $PACKAGES_FILE)
+# Prompt for wireless network information
+read -p "Enter the SSID (network name): " SSID
+read -s -p "Enter the password for $SSID: " PASSWORD
+echo
+
+# Configure wireless connection using wpa_supplicant
+wpa_passphrase "$SSID" "$PASSWORD" > /mnt/etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+
+# Connect to the wireless network
+wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+dhcpcd wlan0
+
+# Ping 1.1.1.1 four times
+artix-chroot /mnt ping -c 4 1.1.1.1
+
+# Basestrap essential packages and network tools for Artix Linux
+basestrap /mnt base base-devel runit elogind-runit os-prober
 
 # Generate fstab
 fstabgen -U /mnt >> /mnt/etc/fstab
 
 # Set hostname
-echo "$HOSTNAME" > /mnt/etc/hostname
+echo "my-artix-install" > /mnt/etc/hostname
 
 # Configure network (install and enable NetworkManager)
-chroot /mnt pacman -S networkmanager
-chroot /mnt ln -s /etc/runit/sv/NetworkManager /etc/runit/runsvdir/default/
+artix-chroot /mnt pacman -S networkmanager networkmanager-runit
+artix-chroot /mnt ln -s /etc/runit/sv/NetworkManager /etc/runit/runsvdir/default/
 
 # Set the root password
-chroot /mnt passwd
+artix-chroot /mnt passwd
 
 # Create a user and add to wheel group for sudo access
-chroot /mnt useradd -m -g users -G wheel -s /bin/bash $USERNAME
-chroot /mnt passwd $USERNAME
+artix-chroot /mnt useradd -m -g users -G wheel -s /bin/bash myuser
+artix-chroot /mnt passwd myuser
 
 # Allow members of the wheel group to execute any command with sudo
 echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
 
 # Install and configure bootloader (GRUB in this example)
-chroot /mnt pacman -S grub
-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+artix-chroot /mnt pacman -S grub os-prober
+
+# Enable os-prober in grub.cfg
+echo "GRUB_DISABLE_OS_PROBER=false" >> /mnt/etc/default/grub
+
+# Detect other operating systems with os-prober
+artix-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+# Identify the EFI partition and install GRUB
+EFI_PARTITION="$(lsblk -f $DRIVE | grep 'EFI System Partition' | awk '{print $1}')"
+artix-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Artix --recheck --removable
 
 # Enable necessary services
-chroot /mnt ln -s /etc/runit/sv/dbus /etc/runit/runsvdir/default/
-chroot /mnt ln -s /etc/runit/sv/elogind /etc/runit/runsvdir/default/
+#artix-chroot /mnt ln -s /etc/runit/sv/dbus /etc/runit/runsvdir/default/
+#artix-chroot /mnt ln -s /etc/runit/sv/elogind /etc/runit/runsvdir/default/
 
 # Optional: Install additional software
-# chroot /mnt pacman -S your-package
+# artix-chroot /mnt pacman -S your-package
 
 # Unmount partitions
 umount -R /mnt
