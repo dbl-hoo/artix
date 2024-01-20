199215528d410ec74ec9296308275ad13e6ff143
dbl-hoo <125836384+dbl-hoo@users.noreply.github.com>
2024-01-20 10:00:04 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,84 +1,101 @@
 #!/bin/bash
 
-# List available drives using lsblk
-lsblk
-echo "Make sure to identify the correct drive for partitioning."
+# Function to print disk information
+print_disk_info() {
+  lsblk
+  echo "Make sure to identify the correct drive for partitioning."
+}
 
-# Prompt for the drive to partition
-read -p "Enter the drive to partition (e.g., /dev/nvme0n1): " DRIVE
+# Function to create and mount partitions
+create_and_mount_partitions() {
+  # Prompt for the drive to partition
+  read -p "Enter the drive to partition (e.g., /dev/nvme0n1): " DRIVE
 
-# List existing partitions on the selected drive
-lsblk $DRIVE
-echo "Make sure to identify the existing partitions on $DRIVE."
+  # List existing partitions on the selected drive
+  lsblk $DRIVE
+  echo "Make sure to identify the existing partitions on $DRIVE."
 
-# Create a new 100GB ext4 partition after the existing root partition
-NEW_PARTITION="$(parted -s $DRIVE mkpart primary ext4 0% 100GB | grep -oE '/dev/[^ ]+')"
+  # Create a new 100GB ext4 partition after the existing root partition
+  NEW_PARTITION="$(parted -s $DRIVE mkpart primary ext4 0% 100GB | grep -oE '/dev/[^ ]+')"
 
-# Format the new partition
-mkfs.ext4 $NEW_PARTITION
+  # Format the new partition
+  mkfs.ext4 $NEW_PARTITION
 
-# Mount the new partition
-mount $NEW_PARTITION /mnt
+  # Mount the new partition
+  mount $NEW_PARTITION /mnt
+}
 
-# Prompt for wireless network information
-read -p "Enter the SSID (network name): " SSID
-read -s -p "Enter the password for $SSID: " PASSWORD
-echo
+# Function to configure wireless network
+configure_wireless_network() {
+  # Prompt for wireless network information
+  read -p "Enter the SSID (network name): " SSID
+  read -s -p "Enter the password for $SSID: " PASSWORD
+  echo
 
-# Configure wireless connection using wpa_supplicant
-wpa_passphrase "$SSID" "$PASSWORD" > /mnt/etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+  # Configure wireless connection using wpa_supplicant
+  wpa_passphrase "$SSID" "$PASSWORD" > /mnt/etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+}
 
-# Connect to the wireless network
-wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-dhcpcd wlan0
+# Function to perform chroot setup
+perform_chroot_setup() {
+  # Basestrap essential packages and network tools for Artix Linux
+  basestrap /mnt base base-devel runit elogind-runit os-prober
 
-# Ping 1.1.1.1 four times
-artix-chroot /mnt ping -c 4 1.1.1.1
+  # Generate fstab
+  fstabgen -U /mnt >> /mnt/etc/fstab
 
-# Basestrap essential packages and network tools for Artix Linux
-basestrap /mnt base base-devel runit elogind-runit os-prober
+  # Prompt for hostname
+  read -p "Enter the desired hostname: " HOSTNAME
+  echo "$HOSTNAME" > /mnt/etc/hostname
 
-# Generate fstab
-fstabgen -U /mnt >> /mnt/etc/fstab
+  # Prompt for username
+  read -p "Enter the desired username: " USERNAME
 
-# Set hostname
-echo "my-artix-install" > /mnt/etc/hostname
+  # Configure network (install and enable NetworkManager)
+  artix-chroot /mnt pacman -S networkmanager wpa_supplicant
+  artix-chroot /mnt ln -s /etc/runit/sv/NetworkManager /etc/runit/runsvdir/default/
 
-# Configure network (install and enable NetworkManager)
-artix-chroot /mnt pacman -S networkmanager networkmanager-runit
-artix-chroot /mnt ln -s /etc/runit/sv/NetworkManager /etc/runit/runsvdir/default/
+  # Set up wpa_supplicant in chroot
+  cp /etc/wpa_supplicant/wpa_supplicant-wlan0.conf /mnt/etc/wpa_supplicant/wpa_supplicant-wlan0.conf
 
-# Set the root password
-artix-chroot /mnt passwd
+  # Connect to the wireless network
+  artix-chroot /mnt wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+  artix-chroot /mnt dhcpcd wlan0
 
-# Create a user and add to wheel group for sudo access
-artix-chroot /mnt useradd -m -g users -G wheel -s /bin/bash myuser
-artix-chroot /mnt passwd myuser
+  # Install and configure bootloader (GRUB in this example)
+  artix-chroot /mnt pacman -S grub os-prober
 
-# Allow members of the wheel group to execute any command with sudo
-echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
+  # Identify the EFI partition and install GRUB
+  EFI_PARTITION="$(lsblk -f $DRIVE | grep 'EFI System Partition' | awk '{print $1}')"
+  artix-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Artix --recheck --removable
 
-# Install and configure bootloader (GRUB in this example)
-artix-chroot /mnt pacman -S grub os-prober
+  # Enable os-prober in grub.cfg
+  echo "GRUB_DISABLE_OS_PROBER=false" >> /mnt/etc/default/grub
 
-# Enable os-prober in grub.cfg
-echo "GRUB_DISABLE_OS_PROBER=false" >> /mnt/etc/default/grub
+  # Detect other operating systems with os-prober
+  artix-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
-# Detect other operating systems with os-prober
-artix-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+  # Allow members of the wheel group to execute any command with sudo
+  echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
 
-# Identify the EFI partition and install GRUB
-EFI_PARTITION="$(lsblk -f $DRIVE | grep 'EFI System Partition' | awk '{print $1}')"
-artix-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Artix --recheck --removable
+  # Set the root password
+  artix-chroot /mnt passwd
 
-# Enable necessary services
-#artix-chroot /mnt ln -s /etc/runit/sv/dbus /etc/runit/runsvdir/default/
-#artix-chroot /mnt ln -s /etc/runit/sv/elogind /etc/runit/runsvdir/default/
+  # Create a user and add to wheel group for sudo access
+  artix-chroot /mnt useradd -m -g users -G wheel -s /bin/bash $USERNAME
+  artix-chroot /mnt passwd $USERNAME
 
-# Optional: Install additional software
-# artix-chroot /mnt pacman -S your-package
+  # Optional: Install additional software
+  # artix-chroot /mnt pacman -S your-package
 
-# Unmount partitions
-umount -R /mnt
+  # Unmount partitions
+  umount -R /mnt
 
-echo "Artix Linux with NetworkManager and additional packages installed successfully!"
+  echo "Artix Linux with NetworkManager and additional packages installed successfully!"
+}
+
+# Main script
+print_disk_info
+create_and_mount_partitions
+configure_wireless_network
+perform_chroot_setup
