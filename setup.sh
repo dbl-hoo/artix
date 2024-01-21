68fc1f73c51ca0f7e628bfa47d734a29419c4565
Jason Kirkham <jason.kirkham@gmail.com>
2024-01-21 18:40:55 -0500

    revisions

--- setup.sh
+++ setup.sh
@@ -9,7 +9,15 @@
 
 #sudo pacman -S --noconfirm artix-archlinux-support
 
+# enable parallel downloads
 sudo sed -i 's/^#\(ParallelDownloads = 5\)/\1/' /etc/pacman.conf
+sed -i 's/^#\(Color\)/\1/' /etc/pacman.conf
+sudo sed -i '/^Color/a ILoveCandy' /etc/pacman.conf
+
+# add arch extra repos
+sed -i '/^\[core\]/a [extra]\nInclude = /etc/pacman.d/mirrorlist-arch\n\n[community]\nInclude = /etc/pacman.d/mirrorlist-arch' /etc/pacman.conf
+
 
 #update repositories
+
 sudo pacman -Syy
