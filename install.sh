527a287558db544055ee08a0f5e7ac996a09c861
dbl-hoo <125836384+dbl-hoo@users.noreply.github.com>
2024-01-21 05:16:27 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -78,7 +78,7 @@ perform_chroot_setup() {
 
   #set the locale
   artix-chroot /mnt echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-  artix-chroot /mnt locale.gen
+  artix-chroot /mnt /etc/locale.gen
   artix-chroot /mnt echo "LANG=en_US.UTF-8" >> /etc/locale.conf
   
   # Prompt for username
@@ -99,6 +99,8 @@ perform_chroot_setup() {
 
   # Detect other operating systems with os-prober
   artix-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+  read -p "Press enter to continue: "
 
   # Allow members of the wheel group to execute any command with sudo
   echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
