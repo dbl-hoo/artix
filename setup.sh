28115f1ea4eb36086fef9c5cd19704f325732e33
Jason Kirkham <jason.kirkham@gmail.com>
2024-01-21 18:23:07 -0500

    revisions

--- setup.sh
+++ setup.sh
@@ -1,19 +1,15 @@
 
 # install yay
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-echo "done"
+#git clone https://aur.archlinux.org/yay.git
+#cd yay
+#makepkg -si
+#echo "done"
 
 # setup arch extra repositories
 
-sudo pacman -S --noconfirm artix-archlinux-support
+#sudo pacman -S --noconfirm artix-archlinux-support
 
-sudo echo "[extra]
-Include = /etc/pacman.d/mirrorlist-arch
-
-[community]
-Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
+sudo sed -i '/^\[options\]/a ParallelDownloads = 5' /etc/pacman.conf
 
 #update repositories
 sudo pacman -Syy
