7e5108e3b672d5abba418d8fa7b28f2e9d0d237d
dbl-hoo <125836384+dbl-hoo@users.noreply.github.com>
2024-01-22 16:42:08 -0500

    added chezmoi init and apply; updated packages to include github-cli

--- 3.sh
+++ 3.sh
@@ -56,3 +56,6 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 # install repo packages
 install_packages
 install_aur_packages
+
+chezmoi init https://github.com/dbl-hoo/dotfiles.git
+chezmoi apply
