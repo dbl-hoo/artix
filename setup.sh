a76016747ae79b17aacedf2c0d61c6fea3d67e38
Jason Kirkham <jason.kirkham@gmail.com>
2024-01-23 08:02:53 -0500

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,29 +1,18 @@
 install_packages() {
-  # Check if packages.txt exists
-  if [ ! -f "packages.txt" ]; then
-    echo "Error: packages.txt not found."
-    return 1
-  fi
 
-  # Read package names from packages.txt and install them
   while IFS= read -r package; do
     sudo pacman -S --noconfirm "$package"
-  done < packages.txt
+  done < ~/install/packages.txt
 
   echo "Installation complete."
 }
 
 install_aur_packages() {
-  # Check if aur_packages.txt exists
-  if [ ! -f "aur_packages.txt" ]; then
-    echo "Error: aur_packages.txt not found."
-    return 1
-  fi
 
   # Read AUR package names from aur_packages.txt and install them using yay
   while IFS= read -r aur_package; do
     yay -S --noconfirm "$aur_package"
-  done < aur_packages.txt
+  done < ~/install/aur_packages.txt
 
   echo "Installation complete."
 }
@@ -33,9 +22,14 @@ install_aur_packages() {
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+cd
 
 # setup arch extra repositories
 sudo pacman -S --noconfirm artix-archlinux-support
+
+#setup terminal with oh-my-zsh, etc.
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 # enable parallel downloads
 sudo sed -i 's/^#\(ParallelDownloads = 5\)/\1/' /etc/pacman.conf
@@ -47,11 +41,6 @@ sudo sed -i '$a [extra]\nInclude = /etc/pacman.d/mirrorlist-arch\n\n[community]\
 
 #update repositories
 sudo pacman -Syy
-
-#setup terminal with oh-my-zsh, etc.
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-
 
 # install repo packages
 install_packages
